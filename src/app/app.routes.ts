f5bfc819405821f2f3855c64456dddb3ded51b92
Your Name <you@example.com>
2026-06-10 11:37:29 -0300

    pagina de cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { Vitrine } from './paginas/vitrine/vitrine';
+import { Login } from './paginas/auth/login/login';
+import { Cadastro } from './paginas/auth/cadastro/cadastro';
 
 export const routes: Routes = [
     {path: "vitrine", component: Vitrine},
-    {path: '', redirectTo: "vitrine", pathMatch: 'full'}
+    {path: '', redirectTo: "vitrine", pathMatch: 'full'},
+    {path: "login", component: Login},
+    {path: "cadastro", component: Cadastro}
 ];
